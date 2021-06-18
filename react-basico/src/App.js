87edd4c5afe60cd47bdbf5812f3a5173955a0cc6
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-18 12:08:22 -0300

    estados de aplicacao

--- react-basico/src/App.js
+++ react-basico/src/App.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Comentario from "./components/Comentario.js";
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Meu Projeto</h1>
-            <Comentario
-                nome="João"
-                email="joao@joao.com"
-                data={new Date(2020, 30, 19)}
-            >
-                Olá, tudo bem?
-            </Comentario>
-            <Comentario
-                nome="Maria"
-                email="maria@maria.com"
-                data={new Date(2020, 30, 21)}
-            >
-                Olá, tudo bem sim!
-            </Comentario>
-        </div>
-    );
+class App extends Component {
+    state = {
+        comentarios: [
+            {
+                nome: "João",
+                email: "joao@joao.com.br",
+                data: new Date(2020, 30, 19),
+                mensagem: "Olá, tudo bem?",
+            },
+            {
+                nome: "Pedro",
+                email: "pedro@pedro.com.br",
+                data: new Date(2020, 30, 22),
+                mensagem: "Olá, tudo bem sim ....",
+            },
+        ],
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Meu Projeto</h1>
+
+                {this.state.comentarios.map((comentario, indice) => (
+                    <Comentario
+                        key={indice}
+                        nome={comentario.nome}
+                        email={comentario.email}
+                        data={comentario.data}
+                    >
+                        {comentario.mensagem}
+                    </Comentario>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default App;
--- react-basico/src/components/Comentario.js
+++ react-basico/src/components/Comentario.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Comentario.css";
 
-const Comentario = (props) => (
-    <div className="Comentario">
-        <h1>{props.nome}</h1>
-        <p>{props.email}</p>
-        <p>{props.children}</p>
-        <p>{props.data.toString()}</p>
-    </div>
-);
+const Comentario = (props) => {
+    return (
+        <div className="Comentario">
+            <h1>{props.nome}</h1>
+            <p>{props.email}</p>
+            <p>{props.children}</p>
+            <p>{props.data.toString()}</p>
+        </div>
+    );
+};
 
 export default Comentario;
