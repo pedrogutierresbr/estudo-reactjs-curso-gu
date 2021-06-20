8dca4d9797ea1ba3ea853d3a2ea7bd1f8eb1b7bc
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-20 17:52:38 -0300

    criado componentes principais

--- modulo-redux/src/App.js
+++ modulo-redux/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import ListaFrutas from "./components/ListaFrutas";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <ListaFrutas />
+        </div>
+    );
 }
 
 export default App;
---
+++ modulo-redux/src/components/AdicionaFruta.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const AdicionaFruta = () => {
+    const [nome, setNome] = useState("");
+    const [quantidade, setQuantidade] = useState(0);
+
+    const adicionarFruta = (event) => {
+        event.preventDefault();
+
+        const fruta = {
+            id: new Date(),
+            nome,
+            quantidade,
+        };
+
+        console.log("Adicionar Fruta", fruta);
+    };
+
+    return (
+        <form onSubmit={adicionarFruta}>
+            <input
+                type="text"
+                name={nome}
+                placeholder="Futa"
+                required
+                onChange={(event) => setNome(event.target.value)}
+            />
+
+            <input
+                type="number"
+                name={quantidade}
+                placeholder="Quantidade"
+                required
+                onChange={(event) => setQuantidade(event.target.value)}
+            />
+
+            <button type="submit">Adicionar</button>
+        </form>
+    );
+};
+
+export default AdicionaFruta;
